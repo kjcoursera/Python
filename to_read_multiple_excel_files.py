b901aa0a6d0290c318a6638d74e9f1fe9d069461
kjcoursera <kanch.upenn@gmail.com>
2018-09-19 12:16:39 -0400

    Add files via upload

---
+++ to_read_multiple_excel_files.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 12:01:33 2018
+
+@author: kanchana
+"""
+
+import glob
+import pandas as pd
+
+# list excel files in a workign directory
+my_files = [f for f in glob.glob("*.xlsx")]
+
+# to get a score column from all sheets
+# assign an empty score variable
+score = [];
+
+# for each files
+for file in my_files:
+    print(file)
+
+# read the file
+    df = pd.read_excel(file)
+
+# extract the rows and columns.
+    data=df.iloc[2:,:]
+# rename the columns 
+    data=data.rename(columns=data.iloc[0]).drop(data.index[0])
+
+#extract the column
+    df2=data.SCORE
+# changing the header based on value in df.columns[1]
+    df2=df2.rename(df.columns[1],inplace =True)
+    score.append(df2)
+
+# to data frame
+final_data = pd.DataFrame(score)
+final_data = final_data.iloc[:,0:14]
